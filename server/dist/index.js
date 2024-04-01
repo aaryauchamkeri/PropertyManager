9294c4539a4b8264a0a20cc12bad06b14d27245d
Aarya Chamkeri <aaryachamkeri06@gmail.com>
2024-03-31 23:29:22 -0700

    test

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,8 +12,9 @@ import { userRouter } from './routes/userRoutes.js';
 import { activityRouter } from './routes/activityRoutes.js';
 const app = express();
 app.use(cors());
-app.use((req, res, next) => {
-    console.log('called');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(express.json());
--- server/src/index.ts
+++ server/src/index.ts
@@ -13,10 +13,11 @@ import { activityRouter } from './routes/activityRoutes.js';
 
 const app = express();
 app.use(cors());
-app.use((req: Request, res: Response, next) => {
-    console.log('called');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
