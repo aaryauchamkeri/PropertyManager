74d43cb7e5277d0803a6558e41a54dd070e4790d
Aarya Chamkeri <137563640+aaryauchamkeri@users.noreply.github.com>
2024-08-23 12:05:56 -0700

    Update getCollection.ts

--- server/src/database/getCollection.ts
+++ server/src/database/getCollection.ts
@@ -1,6 +1,6 @@
 import {Collection, Db, MongoClient} from 'mongodb';
 
-const uri = 'mongodb+srv://aaryachamkeri06:3billionmrr@portfoliopms.9ix6mey.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioPMS';
+const uri = '--------';
 
 const client: MongoClient = new MongoClient(uri);
 const db: Db = client.db("portfoliopms");
